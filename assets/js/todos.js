8b0cf853696ff9998c711b58847ab7092b46bd23
Danqi Liao <danqiliao@gmail.com>
2016-04-20 12:20:11 -0500

    version completed

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("ul").on("click", "li", function(){
+		$(this).toggleClass("completed");
+	})
+
+	//click on X to delete todos
+	$("ul").on("click", "span", function(e){
+		$(this).parent().fadeOut(500,function(){
+			$(this).remove();
+		});
+		e.stopPropagation();		
+	})
+	
+	$("input[type='text']").keypress(function(e){
+		if (e.which === 13){
+			var todoText = $(this).val();
+			$(this).val("");
+			$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText +"</li>")
+		}
+	});
+
+	$(".fa-pencil").click(function(){
+		$("input[type='text']").fadeToggle();
+	});
+
+});
